d00d6fe24b443cb1c8c0027b996d7800a0eb673c
DiegoB <di34nd@gmail.com>
2023-12-08 14:11:40 -0500

    Agregamos margin al div principal

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div className="mt-10 bg-teal-400 rounded-lg">
+    <div className="my-10 bg-teal-400 rounded-lg">
         <div>
             <h1 className=" font-bold uppercase text-center text-6xl sm:p-3 text-indigo-900">Trivia Challenge!</h1>
         </div>
