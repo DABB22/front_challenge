2d27691e29fcae9366c1c7cbd930e169d51a0a1d
DiegoB <di34nd@gmail.com>
2023-12-08 14:17:08 -0500

    Add margin-b to the principal div

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div className="my-10 bg-teal-400 rounded-lg">
+    <div className="py-5 my-10 bg-teal-400 rounded-lg">
         <div>
-            <h1 className=" font-bold uppercase text-center text-6xl sm:p-3 text-indigo-900">Trivia Challenge!</h1>
+            <h1 className=" font-bold uppercase text-center text-6xl text-indigo-900">Trivia Challenge!</h1>
         </div>
 
         <main className="text-center">
